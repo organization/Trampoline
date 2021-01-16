fc4e29eb1b181398e4ea2f6ce557d06cad633f49
PresentKim <debe3721@gmail.com>
2021-01-16 15:31:13 +0900

    style : Formatting the code for contribute

--- src/ifteam/Trampoline/task/fallenTimeOutTask.php
+++ src/ifteam/Trampoline/task/fallenTimeOutTask.php
@@ -2,18 +2,20 @@
 
 namespace ifteam\Trampoline\task;
 
-use pocketmine\scheduler\PluginTask;
 use ifteam\Trampoline\Trampoline;
+use pocketmine\scheduler\PluginTask;
 
-class fallenTimeOutTask extends PluginTask {
-	public $name;
-	public function __construct(Trampoline $owner, $name) {
-		parent::__construct ( $owner );
-		$this->name = $name;
-	}
-	public function onRun($currentTick) {
-		$this->owner->fallenTimeOut ( $this->name );
-	}
+class fallenTimeOutTask extends PluginTask{
+    public $name;
+
+    public function __construct(Trampoline $owner, $name){
+        parent::__construct($owner);
+        $this->name = $name;
+    }
+
+    public function onRun($currentTick){
+        $this->owner->fallenTimeOut($this->name);
+    }
 }
 
 ?>
